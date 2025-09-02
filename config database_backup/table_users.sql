bcbd030ddf162fb343cf08307288f397bb5bef0a
Juan Jose R <jjrealpe.4156@unicesmag.edu.co>
2025-09-02 15:37:00 -0500

    creacion tabla users

---
+++ config database_backup/table_users.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users(
+            id BIGSERIAL PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            mobile_number VARCHAR(20) NOT NULL,
+            ide_number VARCHAR(15) NULL UNIQUE,
+            address TEXT NULL,
+            birthday DATE NULL,
+            email VARCHAR(200) NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            status BOOLEAN NOT NULL DEFAULT TRUE,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+            updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+            deleted_at TIMESTAMPTZ NULL
+
+
+);
+
+insert into users(firstname, lastname, mobile_number, email, password) 
+values('Joan C','Ayala','465133','joan@mail.com','1234');
