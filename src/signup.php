fc7cae3ab2940155f8d7a6471d1ab1f15f48defa
Juan Jose R <jjrealpe.4156@unicesmag.edu.co>
2025-09-11 10:51:38 -0500

    Encryp password

--- src/signup.php
+++ src/signup.php
@@ -1,38 +1,41 @@
 <?php
- //Step 1. get database connection
- require('../config/database.php');
- //Step 2. get form-data
- $f_name = $_POST['fname'];
- $l_name = $_POST['lname'];
- $m_number = $_POST['mnumber'];
- $id_number = $_POST['idnumber'];
- $e_mail = $_POST['email'];
- $p_wd = $_POST['passwd'];
- //Step 3.
- $query = "
- INSERT INTO users (
- firstname,
- lastname,
- mobile_number,
- ide_number,
- email,
- password)
- values (
- '$f_name',
- '$l_name',
- '$m_number',
- '$id_number',
- '$e_mail',
- '$p_wd'
- )
- ";
- //Step 4.
- $res = pg_query($conn, $query);
+  //Step 1. get database connection
+  require('../config/database.php');
+  //Step 2. get form-data
+  $f_name = $_POST['fname'];
+  $l_name = $_POST['lname'];
+  $m_number = $_POST['mnumber'];
+  $id_number = $_POST['idnumber'];
+  $e_mail = $_POST['email'];
+  $p_wd = $_POST['passwd'];
 
- //Step 5. Validation
- if(!$res){
+  $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+
+  //Step 3.
+  $query = "
+  INSERT INTO users (
+  firstname,
+  lastname,
+  mobile_number,
+  ide_number,
+  email,
+  password)
+  values (
+  '$f_name',
+  '$l_name',
+  '$m_number',
+  '$id_number',
+  '$e_mail',
+  '$enc_pass'
+  )
+  ";
+  //Step 4.
+  $res = pg_query($conn, $query);
+
+  //Step 5. Validation
+  if(!$res){
     echo "User has been created successfully !!!";
- } else {
+  } else {
     echo "Something wrong";
- }
+  }
 ?>
