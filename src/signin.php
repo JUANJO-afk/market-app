222d6c0cb36ddf4d81e731952643e468e555cf1f
Juan Jose R <jjrealpe.4156@unicesmag.edu.co>
2025-10-02 11:16:32 -0500

    validation login with md5 go to main

--- src/signin.php
+++ src/signin.php
@@ -3,9 +3,13 @@
   require('../config/database.php');
   //Step 2. get form-data
 
-  $e_mail = $_POST['email'];
-  $p_wd = $_POST['passwd'];
-  //Step 3 Validation
+  $e_mail = trim($_POST['email']);
+  $p_wd = trim($_POST['passwd']);
+  
+  //$enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+  $enc_pass = md5($p_wd);
+
+  //Step 3 Query Validation
 
   $sql_check_user = "
     select 
@@ -15,13 +19,14 @@
 	    users  u
     where 
 	    u.email = '$e_mail'
-	    and u.password = '$p_wd'
+	    and u.password = '$enc_pass'
     limit 1
   ";
   //Step 4
   $res_check = pg_query($conn, $sql_check_user);
   if (pg_num_rows($res_check) > 0){
-    echo "User exists. Go to main !!!";
+    echo "User exists. Go to main !!!";+
+    header('refresh:0;url=main.php'); 
   } else {
     echo "Verify data";
   }
--- src/signup.php
+++ src/signup.php
@@ -3,12 +3,12 @@
   //Step 1. get database connection
   require('../config/database.php');
   //Step 2. get form-data
-  $f_name = $_POST['fname'];
-  $l_name = $_POST['lname'];
-  $m_number = $_POST['mnumber'];
-  $id_number = $_POST['idnumber'];
-  $e_mail = $_POST['email'];
-  $p_wd = $_POST['passwd'];
+  $f_name = trim($_POST['fname']);
+  $l_name = trim($_POST['lname']);
+  $m_number = trim($_POST['mnumber']);
+  $id_number = trim($_POST['idnumber']);
+  $e_mail = trim($_POST['email']);
+  $p_wd = trim($_POST['passwd']);
 
   //$enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
   $enc_pass = md5($p_wd);
@@ -25,27 +25,27 @@
   
   $res_check = pg_query($conn, $check_email);
 
-  if (pg_num_rows($res_check) > 0){
-    echo "<script> alert('Users already exists !!')</script>";+
+  if (pg_num_rows($res_check)>0){
+    echo"<script>alert('Users already exists !!')</script>";+
     header('refresh:0;url=signin.html');
   } else{
       //Step 3. Create query to insert into
       $query = "
           INSERT INTO users (
-          firstname,
-          lastname,
-          mobile_number,
-          ide_number,
-          email,
-          password)
-        values (
-          '$f_name',
-          '$l_name',
-          '$m_number',
-          '$id_number',
-          '$e_mail',
-          '$enc_pass'
-        )
+            firstname,
+            lastname,
+            mobile_number,
+            ide_number,
+            email,
+            password)
+          values (
+            '$f_name',
+            '$l_name',
+            '$m_number',
+            '$id_number',
+            '$e_mail',
+            '$enc_pass'
+          )
       ";
     //Step 4.
     $res = pg_query($conn, $query);
@@ -53,8 +53,8 @@
     //Step 5. Validation
     if($res){
       //echo "User has been created successfully !!!";
-      echo "<script> alert('Sucess !!! Go to login')</script>";
-      header('refresh : 0 ; url = signin.html');
+      echo"<script>alert('Sucess !!! Go to login')</script>";+
+      header('refresh:0;url=signin.html');
     } else {
       echo "Something wrong";
     }
