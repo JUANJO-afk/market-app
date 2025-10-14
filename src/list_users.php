89bc68c3dec84546fcbb146991daf05027a4aef1
Juan Jose R <jjrealpe.4156@unicesmag.edu.co>
2025-10-14 15:41:34 -0500

    List users ui

---
+++ src/list_users.php
@@ -0,0 +1,34 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border ="1" align = "center">
+        <tr>
+            <th>Fullname </th>    
+            <th>E-mail </th>    
+            <th>Ide. number </th>    
+            <th>Phone number </th>
+            <th>Status</th>    
+            <th>Options </th>    
+        </tr>
+        <tr>
+            <td>Joan </td>    
+            <td>joa@gmail.com </td>    
+            <td>12315487 </td>    
+            <td>5456132</td>    
+            <td>Active </td>    
+            <td>
+                    <a href = "#"><img  src = "icons/lupa.png" width = "20"><a>
+                    <a href = "#">Update<a>
+                    <a href = "#">Delete<a>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
